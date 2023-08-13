f7a729c37a7c78c41bf06e4070c09d01a47bb74b
trenega <niijimatakashi993@icloud.com>
2023-08-13 14:20:32 +0900

    :add: 新規機能・新規ファイル追加
     Changes to be committed:
    	modified:   init.el
    
    add:
    
    Settings:
    ivy.el
    counsel.el
    swiper.el

--- init.el
+++ init.el
@@ -12,18 +12,68 @@
     (goto-char (point-max))
     (eval-print-last-sexp)))
 
-;; el-get Initial settings-------------------------------
+;; End el-get Initial settings-------------------------------
 
 ;; el-get Install packages-------------------------------
 (el-get-bundle counsel)
 
 ; swiper のリポジトリを指定
-(el-get-bundle "abo-abo/swiper")
+; (el-get-bundle "abo-abo/swiper")
 
 ;; End el-get Install packages---------------------------
 
+;; ivy Setting-------------------------------------------
+; refs: https://takaxp.github.io/articles/qiita-helm2ivy.html
+(when (require 'ivy nil t)
 
+  ;; M-o を ivy-hydra-read-action に割り当てる．
+  (when (require 'ivy-hydra nil t)
+    (setq ivy-read-action-function #'ivy-hydra-read-action))
 
+  ;; `ivy-switch-buffer' (C-x b) のリストに recent files と bookmark を含める．
+  (setq ivy-use-virtual-buffers t)
+
+  ;; ミニバッファでコマンド発行を認める
+  (when (setq enable-recursive-minibuffers t)
+    (minibuffer-depth-indicate-mode 1)) ;; 何回層入ったかプロンプトに表示．
+
+  ;; ESC連打でミニバッファを閉じる
+  (define-key ivy-minibuffer-map (kbd "<escape>") 'minibuffer-keyboard-quit)
+
+  ;; プロンプトの表示が長い時に折り返す（選択候補も折り返される）
+  (setq ivy-truncate-lines nil)
+
+  ;; リスト先頭で `C-p' するとき，リストの最後に移動する
+  (setq ivy-wrap t)
+
+  ;; アクティベート
+  (ivy-mode 1))
+
+;; End ivy Setting---------------------------------------
+
+;; counsel Setting---------------------------------------
+(when (require 'counsel nil t)
+
+  ;; キーバインドは一例です．好みに変えましょう．
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "M-y") 'counsel-yank-pop)
+  (global-set-key (kbd "C-M-z") 'counsel-fzf)
+  (global-set-key (kbd "C-M-r") 'counsel-recentf)
+  (global-set-key (kbd "C-x C-b") 'counsel-ibuffer)
+  (global-set-key (kbd "C-M-f") 'counsel-ag)
+
+  ;; アクティベート
+  (counsel-mode 1))
+
+;; End counsel Setting-----------------------------------
+
+;; swiper Setting----------------------------------------
+(when (require 'swiper nil t)
+
+  ;; キーバインドは一例です．好みに変えましょう．
+  (global-set-key (kbd "M-s M-s") 'swiper-thing-at-point))
+
+;; End swiper Setting------------------------------------
 
 ;;
 ;; 基本設定
