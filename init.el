c202e529d39e0226f13cd93de346c0e26c830154
trenega <niijimatakashi993@icloud.com>
2023-09-05 07:48:31 +0900

    :add: 新規機能・新規ファイル追加
     Changes to be committed:
    	modified:   init.el
    
    add:
    
    ivy settings

--- init.el
+++ init.el
@@ -154,9 +154,36 @@
 
 ;;; End straight.el---------------------------------------
 
-;;;-----------------------------------------------------
+;;;-------------------------------------------------------
+;;; Pagckage Settings
+;;;-------------------------------------------------------
+
+;; ivy
+(when (require 'ivy nil t)
+
+  ;; M-o を ivy-dispatching-done-hydra に割り当てる．
+  (require 'ivy-hydra)
+
+  ;; `ivy-switch-buffer' (C-x b) のリストに recent files と bookmark を含める．
+  (setq ivy-use-virtual-buffers t)
+
+  ;; ミニバッファでコマンド発行を認める
+  (when (setq enable-recursive-minibuffers t)
+    (minibuffer-depth-indicate-mode 1)) ;; 何回層入ったかプロンプトに表示．
+
+  ;; ESC連打でミニバッファを閉じる
+  (define-key ivy-minibuffer-map (kbd "<escape>") 'minibuffer-keyboard-quit)
+
+  ;; アクティベート
+  (ivy-mode 1))
+
+;;;-------------------------------------------------------
+;;; End Pagckage Settings
+;;;-------------------------------------------------------
+
+;;;-------------------------------------------------------
 ;;; Color
-;;;-----------------------------------------------------
+;;;-------------------------------------------------------
 
 ;; 画面を黒く設定する
 ;; refs: kei10in.hatenablog.jp/entry/20101101/1288617632
