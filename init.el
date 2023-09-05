dbeb736492fd663dac2ff427ad1497aff0c5d195
trenega <niijimatakashi993@icloud.com>
2023-09-05 09:08:48 +0900

    :add: 新規機能・新規ファイル追加
     Changes to be committed:
    	modified:   init.el
    
     Untracked files:
    add:
    
    cousel Settings

--- init.el
+++ init.el
@@ -184,6 +184,21 @@
 
   ;; アクティベート
   (ivy-mode 1))
+
+;; counsel Settings
+(when (require 'counsel nil t)
+
+  ;; キーバインドは一例です．好みに変えましょう．
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "M-y") 'counsel-yank-pop)
+  (global-set-key (kbd "C-M-z") 'counsel-fzf)
+  (global-set-key (kbd "C-M-r") 'counsel-recentf)
+  (global-set-key (kbd "C-x C-b") 'counsel-ibuffer)
+  (global-set-key (kbd "C-M-f") 'counsel-ag)
+
+  ;; アクティベート
+  (counsel-mode 1))
+
 ;;;-------------------------------------------------------
 ;;; End Pagckage Settings
 ;;;-------------------------------------------------------
