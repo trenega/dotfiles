16f549d0b2d5245b24fbdb9ad4302be3bff26ada
trenega <niijimatakashi993@icloud.com>
2023-09-05 09:04:52 +0900

    :add: 新規機能・新規ファイル追加
     Changes to be committed:
    	modified:   init.el
    
     Untracked files:
    add:
    
    ivy settings

--- init.el
+++ init.el
@@ -159,10 +159,12 @@
 ;;;-------------------------------------------------------
 
 ;; ivy
+;; refs: https://takaxp.github.io/articles/qiita-helm2ivy.html
 (when (require 'ivy nil t)
 
-  ;; M-o を ivy-dispatching-done-hydra に割り当てる．
-  (require 'ivy-hydra)
+  ;; M-o を ivy-hydra-read-action に割り当てる．
+  (when (require 'ivy-hydra nil t)
+    (setq ivy-read-action-function #'ivy-hydra-read-action))
 
   ;; `ivy-switch-buffer' (C-x b) のリストに recent files と bookmark を含める．
   (setq ivy-use-virtual-buffers t)
@@ -174,9 +176,14 @@
   ;; ESC連打でミニバッファを閉じる
   (define-key ivy-minibuffer-map (kbd "<escape>") 'minibuffer-keyboard-quit)
 
+  ;; プロンプトの表示が長い時に折り返す（選択候補も折り返される）
+  (setq ivy-truncate-lines nil)
+
+  ;; リスト先頭で `C-p' するとき，リストの最後に移動する
+  (setq ivy-wrap t)
+
   ;; アクティベート
   (ivy-mode 1))
-
 ;;;-------------------------------------------------------
 ;;; End Pagckage Settings
 ;;;-------------------------------------------------------
